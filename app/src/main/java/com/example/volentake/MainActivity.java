da0ea5cc1991fb1c7a25d2bf9e36a586728bf8ea
kggold4 <68167734+kggold4@users.noreply.github.com>
2021-11-14 16:26:55 +0200

    add design to VolunteerLogin page

--- app/src/main/java/com/example/volentake/MainActivity.java
+++ app/src/main/java/com/example/volentake/MainActivity.java
@@ -3,9 +3,7 @@ package com.example.volentake;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -36,11 +34,5 @@ public class MainActivity extends AppCompatActivity {
             Intent intent = new Intent(MainActivity.this, GuestUser.class);
             startActivity(intent);
         });
-
-
     }
-
-
-
-
 }
--- app/src/main/java/com/example/volentake/VolunteerLogIn.java
+++ app/src/main/java/com/example/volentake/VolunteerLogIn.java
@@ -1,13 +1,38 @@
 package com.example.volentake;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class VolunteerLogIn extends AppCompatActivity {
+
+    private Button volunteerLoginBtn;
+    private Button volunteerCreateUser;
+    private Button volunteerLoginBack;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_volunteer_log_in);
+
+        volunteerLoginBtn = (Button) findViewById(R.id.volunteerloginbtn);
+        volunteerCreateUser = (Button) findViewById(R.id.volunteercreateuser);
+        volunteerLoginBack = (Button) findViewById(R.id.volunteerloginback);
+
+        volunteerLoginBtn.setOnClickListener(view -> {
+            Intent intent = new Intent(VolunteerLogIn.this, VolunteerPage.class);
+            startActivity(intent);
+        });
+
+//        volunteerCreateUser.setOnClickListener(view -> {
+//            Intent intent = new Intent(VolunteerLogIn.this, VolunteerUserSignUp.class);
+//            startActivity(intent);
+//        });
+
+        volunteerLoginBack.setOnClickListener(view -> {
+            Intent intent = new Intent(VolunteerLogIn.this, MainActivity.class);
+            startActivity(intent);
+        });
     }
 }
