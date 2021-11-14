680b6cc712a00fd502ccc3b1cfa42d369b177a5b
kggold4 <68167734+kggold4@users.noreply.github.com>
2021-11-14 16:39:54 +0200

    improve design in VolunteerLogin page

--- app/src/main/java/com/example/volentake/VolunteerLogIn.java
+++ app/src/main/java/com/example/volentake/VolunteerLogIn.java
@@ -4,12 +4,18 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class VolunteerLogIn extends AppCompatActivity {
 
+    // buttons
     private Button volunteerLoginBtn;
     private Button volunteerCreateUser;
     private Button volunteerLoginBack;
+
+    // insert details for user signIn
+    private TextView volunteerEmailEditText;
+    private TextView volunteerPasswordEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,7 +26,16 @@ public class VolunteerLogIn extends AppCompatActivity {
         volunteerCreateUser = (Button) findViewById(R.id.volunteercreateuser);
         volunteerLoginBack = (Button) findViewById(R.id.volunteerloginback);
 
+        volunteerEmailEditText = (TextView) findViewById(R.id.emailtext);
+        volunteerPasswordEditText = (TextView) findViewById(R.id.passwordtext);
+
         volunteerLoginBtn.setOnClickListener(view -> {
+
+            // checking validate of user details with firebase database - email + password
+            // ***
+            String email = volunteerEmailEditText.getText().toString();
+            String password = volunteerPasswordEditText.getText().toString();
+
             Intent intent = new Intent(VolunteerLogIn.this, VolunteerPage.class);
             startActivity(intent);
         });
