07a491303092dfc7c0f90b59dee44dcf46dc6026
Dvir-Segal <dvirsegal96@gmail.com>
2021-11-17 16:45:42 +0200

    firebase

--- app/src/main/java/com/example/volentake/VolunteerUserSignUp.java
+++ app/src/main/java/com/example/volentake/VolunteerUserSignUp.java
@@ -2,12 +2,20 @@ package com.example.volentake;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class VolunteerUserSignUp extends AppCompatActivity {
+
+    private Button volunteerCreateUser;
+    private Button toVolunteerLogIn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_volunteer_user_sign_up);
+
+        volunteerCreateUser = (Button) findViewById(R.id.btnCreateVolunteer);
+        toVolunteerLogIn = (Button) findViewById(R.id.toVolunteerLogIn);
+
     }
 }
