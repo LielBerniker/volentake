158edb830350dcfefa6b1f66f1662e14a3d54941
Dvir-Segal <dvirsegal96@gmail.com>
2021-11-30 12:18:42 +0200

    but

--- app/src/main/java/com/example/myapplication/AdapterPost.java
+++ app/src/main/java/com/example/myapplication/AdapterPost.java
@@ -7,6 +7,7 @@ import android.view.ViewGroup;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.cardview.widget.CardView;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.volentake.R;
@@ -16,10 +17,11 @@ import com.google.firebase.database.FirebaseDatabase;
 import java.util.ArrayList;
 
 public class AdapterPost extends RecyclerView.Adapter<AdapterPost.ViewHolder> {
+    Context context;
     ArrayList<Assoc_post> listPosts= new ArrayList<>();
 
-    public AdapterPost(Context context, ArrayList<Assoc_post> listPosts){
-        this.listPosts = listPosts;
+    public AdapterPost(Context context){
+        this.context = context;
     }
 
     @Override
@@ -31,17 +33,8 @@ public class AdapterPost extends RecyclerView.Adapter<AdapterPost.ViewHolder> {
 
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
-//        final String uid = modelPosts.get(position).getUid();
-//        String nameh = modelPosts.get(position).getUname();
-//        final String titlee = modelPosts.get(position).getTitle();
-//        final String descri = modelPosts.get(position).getDescription();
-//        final String ptime = modelPosts.get(position).getPtime();
-//        String dp = modelPosts.get(position).getUdp();
-//        String plike = modelPosts.get(position).getPlike();
-//        final String image = modelPosts.get(position).getUimage();
-//        String email = modelPosts.get(position).getUemail();
-//        String comm = modelPosts.get(position).getPcomments();
-//        final String pid = modelPosts.get(position).getPtime();
+        holder.txtNameAssociation.setText(listPosts.get(position).getName());
+        holder.txtNumVol.setText(listPosts.get(position).getNum_of_participants());
 
     }
 
@@ -56,9 +49,14 @@ public class AdapterPost extends RecyclerView.Adapter<AdapterPost.ViewHolder> {
     }
     public class ViewHolder extends RecyclerView.ViewHolder{
         private TextView txtNameAssociation, txtNumVol;
+        private CardView parent;
 
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
+            txtNameAssociation = itemView.findViewById(R.id.nameAss);
+            txtNumVol = itemView.findViewById(R.id.numVol);
+            parent = itemView.findViewById(R.id.parent);
+
         }
     }
 }
