df2d3ae5403b6f51d1cc60c804fd2a67dd574a09
liel berniker <lielb101995@gmail.com>
2021-12-15 14:10:53 +0200

    vol user inbox edit

--- app/src/main/java/com/example/volentake/InboxResponses.java
+++ app/src/main/java/com/example/volentake/InboxResponses.java
@@ -1,16 +1,22 @@
 package com.example.volentake;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.util.Pair;
 
 import com.example.myapplication.AdapterReqAssociation;
 import com.example.myapplication.AdapterResponse;
+import com.example.myapplication.Assoc_user;
 import com.example.myapplication.Request_vol;
 import com.example.myapplication.Response;
+import com.example.myapplication.Vol_user;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -23,9 +29,10 @@ public class InboxResponses extends AppCompatActivity {
 
     private RecyclerView recycleResponses;
     private DatabaseReference mRootRef;
-    String response_id = "";
+    String vol_id = "";
+    ArrayList<String> cur_responses ;
     ArrayList<Pair<Response,String>> responses = new ArrayList<>();
-
+    int num_of_responses;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -34,36 +41,53 @@ public class InboxResponses extends AppCompatActivity {
         bun = getIntent().getExtras();
         if(bun != null)
         {
-            response_id = bun.getString("id");
+            vol_id = bun.getString("id");
         }
 
         recycleResponses = findViewById(R.id.recycleResponses);
         mRootRef = FirebaseDatabase.getInstance().getReference();
 
-        mRootRef.child("posts")
-                .addListenerForSingleValueEvent(new ValueEventListener() {
-                    @Override
-                    public void onDataChange(DataSnapshot dataSnapshot) {
-                        ///////for Liel
-//                        for (DataSnapshot data1 : dataSnapshot.getChildren() )
-//                        {
-//                            String post_id = data1.getKey();
-//                            Assoc_post cur_post1 = data1.getValue(Assoc_post.class);
-//                            Assoc_post cur_post2 = new Assoc_post(cur_post1.getName(),cur_post1.getLocation(),cur_post1.getNum_of_participants(),cur_post1.getType(),cur_post1.getPhone_number(), cur_post1.userId,cur_post1.getDescription());
-//                            Pair<Assoc_post,String> pair1 =new Pair<>(cur_post2,post_id);
-//                            posts.add(pair1);
-//
-//                        }
-                        AdapterResponse adapter = new AdapterResponse( InboxResponses.this,response_id);
-                        adapter.setResponses(responses);
+        mRootRef.child("vol_users").child(vol_id).get().addOnCompleteListener(new OnCompleteListener<DataSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DataSnapshot> task) {
+                if (!task.isSuccessful()) {
+                    Log.e("firebase", "Error getting data", task.getException());
+                }
+                else {
+                    Vol_user cure_user =  task.getResult().getValue(Vol_user.class);
+                    cur_responses= cure_user.getMassages_res();
 
-                        recycleResponses.setAdapter(adapter);
-                        recycleResponses.setLayoutManager(new GridLayoutManager(InboxResponses.this,1));
+                    num_of_responses = cur_responses.size();
+                    for (int i = 1; i <num_of_responses ; i++) {
+                        int count = i;
+                        String res_id = cur_responses.get(count);
+                        mRootRef.child("massage_vol").child(res_id).get().addOnCompleteListener(new OnCompleteListener<DataSnapshot>() {
+                            @Override
+                            public void onComplete(@NonNull Task<DataSnapshot> task) {
+                                if (!task.isSuccessful()) {
+                                    Log.e("firebase", "Error getting data", task.getException());
+                                }
+                                else {
+                                    Response cur_res1 =  task.getResult().getValue(Response.class);
+                                    Response cur_res2 = new Response(cur_res1.getVol_user_id(),cur_res1.getAssociation_user_id(),cur_res1.getPost_id(),cur_res1.getStatus());
+                                    Pair<Response,String> pair1 =new Pair<>(cur_res2,res_id);
+                                    responses.add(pair1);
+                                    if(count+1==num_of_responses)
+                                    {
+                                        AdapterResponse adapter = new AdapterResponse( InboxResponses.this,vol_id);
+                                        adapter.setResponses(responses);
+
+                                        recycleResponses.setAdapter(adapter);
+                                        recycleResponses.setLayoutManager(new GridLayoutManager(InboxResponses.this,1));}
+                                }
+                            }
+                        });
                     }
-                    @Override
-                    public void onCancelled(DatabaseError databaseError) {
-                    }
-                });
+
+
+                }
+            }
+        });
 
 
 
