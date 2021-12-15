9fea52bd01d179b94488c147b7074beda56e511e
kggold4 <68167734+kggold4@users.noreply.github.com>
2021-12-15 19:04:55 +0200

    add volunteer details for assoc activity and design others xml files

---
+++ app/src/main/java/com/example/volentake/VolunteerDetailsForAssoc.java
@@ -0,0 +1,14 @@
+package com.example.volentake;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class VolunteerDetailsForAssoc extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_volunteer_details_for_assoc);
+    }
+}
