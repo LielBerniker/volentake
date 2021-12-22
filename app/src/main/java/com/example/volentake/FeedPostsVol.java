57a1e4060847d17da188e23ba865cfc5296971b8
kggold4 <68167734+kggold4@users.noreply.github.com>
2021-12-23 01:05:02 +0200

    update some design nd add back btns

--- app/src/main/java/com/example/volentake/FeedPostsVol.java
+++ app/src/main/java/com/example/volentake/FeedPostsVol.java
@@ -5,8 +5,10 @@ import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.app.ProgressDialog;
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Pair;
+import android.widget.Button;
 
 import com.example.myapplication.AdapterPostVol;
 import com.example.myapplication.Assoc_post;
@@ -22,17 +24,19 @@ public class FeedPostsVol extends AppCompatActivity {
 
     private RecyclerView postsRecycle;
     private DatabaseReference mRootRef;
+    private Button backBtn;
     String vol_user_id = "";
-    ArrayList<Pair<Assoc_post,String>> posts = new ArrayList<>();
+    ArrayList<Pair<Assoc_post, String>> posts = new ArrayList<>();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_feed_posts_vol);
+        backBtn = findViewById(R.id.back_activity_feed_posts_vol);
 //        get information bundle
         Bundle bun = null;
         bun = getIntent().getExtras();
-        if(bun != null)
-        {
+        if (bun != null) {
             vol_user_id = bun.getString("id");
         }
         postsRecycle = findViewById(R.id.recyclePosts);
@@ -46,29 +50,33 @@ public class FeedPostsVol extends AppCompatActivity {
                 .addListenerForSingleValueEvent(new ValueEventListener() {
                     @Override
                     public void onDataChange(DataSnapshot dataSnapshot) {
-                      for (DataSnapshot data1 : dataSnapshot.getChildren() )
-                      {
-                          String post_id = data1.getKey();
-                          Assoc_post cur_post1 = data1.getValue(Assoc_post.class);
-                          Assoc_post cur_post2 = new Assoc_post(cur_post1.getName(),cur_post1.getLocation(),cur_post1.getNum_of_participants(),cur_post1.getType(),cur_post1.getPhone_number(), cur_post1.userId,cur_post1.getDescription());
-                          Pair<Assoc_post,String> pair1 =new Pair<>(cur_post2,post_id);
-                          posts.add(pair1);
+                        for (DataSnapshot data1 : dataSnapshot.getChildren()) {
+                            String post_id = data1.getKey();
+                            Assoc_post cur_post1 = data1.getValue(Assoc_post.class);
+                            Assoc_post cur_post2 = new Assoc_post(cur_post1.getName(), cur_post1.getLocation(), cur_post1.getNum_of_participants(), cur_post1.getType(), cur_post1.getPhone_number(), cur_post1.userId, cur_post1.getDescription());
+                            Pair<Assoc_post, String> pair1 = new Pair<>(cur_post2, post_id);
+                            posts.add(pair1);
 
-                      }
-                        AdapterPostVol adapter = new AdapterPostVol( FeedPostsVol.this,vol_user_id,"",2);
+                        }
+                        AdapterPostVol adapter = new AdapterPostVol(FeedPostsVol.this, vol_user_id, "", 2);
                         adapter.setPosts(posts);
 
                         postsRecycle.setAdapter(adapter);
-                        postsRecycle.setLayoutManager(new GridLayoutManager(FeedPostsVol.this,1));
+                        postsRecycle.setLayoutManager(new GridLayoutManager(FeedPostsVol.this, 1));
                         progressDialog.dismiss();
                     }
+
                     @Override
                     public void onCancelled(DatabaseError databaseError) {
                     }
                 });
 
 
-
+        backBtn.setOnClickListener(view -> {
+            Intent intent = new Intent(FeedPostsVol.this, DetailsPostVol.class);
+            intent.putExtra("id", vol_user_id);
+            startActivity(intent);
+        });
     }
 
 }
