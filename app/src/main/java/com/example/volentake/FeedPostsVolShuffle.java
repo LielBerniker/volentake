8b7ae1dfee27f965fd316110d58d745413e1966d
Dvir-Segal <dvirsegal96@gmail.com>
2021-12-23 14:02:44 +0200

    FeedShuffle

---
+++ app/src/main/java/com/example/volentake/FeedPostsVolShuffle.java
@@ -0,0 +1,143 @@
+package com.example.volentake;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.app.ProgressDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Pair;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Button;
+
+import com.example.myapplication.AdapterPostVol;
+import com.example.myapplication.Assoc_post;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class FeedPostsVolShuffle extends AppCompatActivity {
+
+    private RecyclerView recyclePostsVolShuffle;
+    private DatabaseReference mRootRef;
+    private Button backBtn;
+    AlertDialog.Builder builder;
+    String vol_user_id = "";
+    ArrayList<Pair<Assoc_post, String>> posts = new ArrayList<>();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_feed_posts_vol_shuffle);
+        backBtn = findViewById(R.id.back_activity_feed_posts_vol_shuffle);
+        builder = new AlertDialog.Builder(this);
+        ProgressDialog progressDialog
+                = new ProgressDialog(this);
+        progressDialog.setTitle("loading...");
+        progressDialog.setCancelable(false);
+        progressDialog.setIcon(R.drawable.logovector_01);
+        progressDialog.show();
+        Bundle bun = null;
+        bun = getIntent().getExtras();
+        if (bun != null) {
+            vol_user_id = bun.getString("id");
+        }
+        recyclePostsVolShuffle = findViewById(R.id.recyclePostsVolShuffle);
+        mRootRef = FirebaseDatabase.getInstance().getReference();
+
+        mRootRef.child("posts")
+                .addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(DataSnapshot dataSnapshot) {
+                        for (DataSnapshot data1 : dataSnapshot.getChildren()) {
+                            String post_id = data1.getKey();
+                            Assoc_post cur_post1 = data1.getValue(Assoc_post.class);
+                            Assoc_post cur_post2 = new Assoc_post(cur_post1.getName(), cur_post1.getLocation(), cur_post1.getNum_of_participants(), cur_post1.getType(), cur_post1.getPhone_number(), cur_post1.userId, cur_post1.getDescription());
+                            Pair<Assoc_post, String> pair1 = new Pair<>(cur_post2, post_id);
+                            posts.add(pair1);
+
+                        }
+                        if (posts.size() == 0) {
+                            progressDialog.dismiss();
+                            builder.setTitle("volunteering events")
+                                    .setMessage("there is not any volunteering events")
+                                    .setCancelable(true)
+                                    .setPositiveButton("ok", new DialogInterface.OnClickListener() {
+                                        @Override
+                                        public void onClick(DialogInterface dialog, int which) {
+                                            Intent intent1 = new Intent(FeedPostsVolShuffle.this, MainSearchPostsVol.class);
+                                            intent1.putExtra("id", vol_user_id);
+                                            startActivity(intent1);
+                                        }
+                                    });
+                            AlertDialog alterdialod = builder.create();
+                            alterdialod.show();
+                        }
+                        Collections.shuffle(posts);
+                        AdapterPostVol adapter = new AdapterPostVol(FeedPostsVolShuffle.this, vol_user_id, "", 0);
+                        adapter.setPosts(posts);
+
+                        recyclePostsVolShuffle.setAdapter(adapter);
+                        recyclePostsVolShuffle.setLayoutManager(new GridLayoutManager(FeedPostsVolShuffle.this, 1));
+                        progressDialog.dismiss();
+                    }
+
+                    @Override
+                    public void onCancelled(DatabaseError databaseError) {
+                    }
+                });
+        backBtn.setOnClickListener(view -> {
+            Intent intent = new Intent(FeedPostsVolShuffle.this, MainSearchPostsVol.class);
+            intent.putExtra("id", vol_user_id);
+            startActivity(intent);
+        });
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_bar, menu);
+        return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.itembacktouser:
+                Intent intent1 = new Intent(FeedPostsVolShuffle.this,VolunteerPage.class);
+                intent1.putExtra("id",vol_user_id);
+                startActivity(intent1);
+                return true;
+            case R.id.itemlogout:
+                builder.setTitle("log out")
+                        .setMessage("are you sure you want to log out?")
+                        .setCancelable(true)
+                        .setPositiveButton("yes", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                Intent intent1 = new Intent(FeedPostsVolShuffle.this, VolunteerLogIn.class);
+                                startActivity(intent1);
+                            }
+                        })
+                        .setNegativeButton("cancel", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                dialog.cancel();
+                            }
+                        });
+                AlertDialog alterdialod = builder.create();
+                alterdialod.show();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+}
