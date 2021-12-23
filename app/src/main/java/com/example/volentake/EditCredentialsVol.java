ccb4a12506bbca439ecddb62fbbf6ddea2130b33
kggold4 <68167734+kggold4@users.noreply.github.com>
2021-12-23 13:06:44 +0200

    add back buttons

---
+++ app/src/main/java/com/example/volentake/EditCredentialsVol.java
@@ -0,0 +1,14 @@
+package com.example.volentake;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class EditCredentialsVol extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_credentials_vol);
+    }
+}
