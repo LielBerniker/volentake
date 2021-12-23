14236d36a0e9f487a7cb2fce8e4bf29928660580
kggold4 <68167734+kggold4@users.noreply.github.com>
2021-12-23 23:02:56 +0200

    big design update

--- app/src/main/java/com/example/volentake/DetailsPostAssociation.java
+++ app/src/main/java/com/example/volentake/DetailsPostAssociation.java
@@ -35,15 +35,16 @@ import java.io.File;
 import java.io.IOException;
 
 public class DetailsPostAssociation extends AppCompatActivity {
-    private TextView PostName,PostCity,PostStreet,Poststnum,NumOfVol,PostType,PostPhoneNum, PostDescription;
-    private Button back, btnForEditPost,editpic;
+    private TextView PostName, PostCity, PostStreet, Poststnum, NumOfVol, PostType, PostPhoneNum, PostDescription;
+    private Button back, btnForEditPost, editPic;
     private ImageView post_pic;
     //    firebase
     private DatabaseReference mDatabase;
-    private StorageReference mystorge;
+    private StorageReference myStorage;
     AlertDialog.Builder builder;
     String association_user_id = "";
     String post_id = "";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -61,29 +62,27 @@ public class DetailsPostAssociation extends AppCompatActivity {
 //        get information bundle
         Bundle bun = null;
         bun = getIntent().getExtras();
-        if(bun != null)
-        {
+        if (bun != null) {
             association_user_id = bun.getString("assoc_id");
             post_id = bun.getString("post_id");
         }
-        mystorge = FirebaseStorage.getInstance().getReference().child("post_description_pic/"+post_id);
+        myStorage = FirebaseStorage.getInstance().getReference().child("post_description_pic/" + post_id);
         back = (Button) findViewById(R.id.btnBackToFeedOfPostsAss);
-        editpic = (Button) findViewById(R.id.btneditpostpic);
-        btnForEditPost =  (Button) findViewById(R.id.btnPageEditPostsAss);
-        PostName = (TextView)findViewById(R.id.detailPostNameAss);
-        NumOfVol = (TextView)findViewById(R.id.detailNumVolPostAss);
-        PostPhoneNum = (TextView)findViewById(R.id.detailPhoneNumberPostAss);
-        PostDescription = (TextView)findViewById(R.id.detailDescriptionPostAss);
-        PostType = (TextView)findViewById(R.id.detailTypePostAss);
-        post_pic = (ImageView)findViewById(R.id.PostImage2Ass);
+        editPic = (Button) findViewById(R.id.btneditpostpic);
+        btnForEditPost = (Button) findViewById(R.id.btnPageEditPostsAss);
+        PostName = (TextView) findViewById(R.id.detailPostNameAss);
+        NumOfVol = (TextView) findViewById(R.id.detailNumVolPostAss);
+        PostPhoneNum = (TextView) findViewById(R.id.detailPhoneNumberPostAss);
+        PostDescription = (TextView) findViewById(R.id.detailDescriptionPostAss);
+        PostType = (TextView) findViewById(R.id.detailTypePostAss);
+        post_pic = (ImageView) findViewById(R.id.PostImage2Ass);
         mDatabase.child("posts").child(post_id).get().addOnCompleteListener(new OnCompleteListener<DataSnapshot>() {
             @Override
             public void onComplete(@NonNull Task<DataSnapshot> task) {
                 if (!task.isSuccessful()) {
                     Log.e("firebase", "Error getting data", task.getException());
-                }
-                else {
-                    Assoc_post cur_post =  task.getResult().getValue(Assoc_post.class);
+                } else {
+                    Assoc_post cur_post = task.getResult().getValue(Assoc_post.class);
                     assert cur_post != null;
                     PostName.setText(cur_post.getName());
                     NumOfVol.setText(String.valueOf(cur_post.getNum_of_participants()));
@@ -93,12 +92,12 @@ public class DetailsPostAssociation extends AppCompatActivity {
                 }
             }
         });
-        mystorge.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+        myStorage.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
             @Override
             public void onSuccess(Uri uri) {
                 try {
-                    final File localFile = File.createTempFile(post_id,"");
-                    mystorge.getFile(localFile)
+                    final File localFile = File.createTempFile(post_id, "");
+                    myStorage.getFile(localFile)
                             .addOnSuccessListener(new OnSuccessListener<FileDownloadTask.TaskSnapshot>() {
                                 @Override
                                 public void onSuccess(FileDownloadTask.TaskSnapshot taskSnapshot) {
@@ -112,8 +111,7 @@ public class DetailsPostAssociation extends AppCompatActivity {
                         public void onFailure(@NonNull Exception e) {
                         }
                     });
-                }
-                catch (IOException e){
+                } catch (IOException e) {
 
                 }
             }
@@ -125,35 +123,37 @@ public class DetailsPostAssociation extends AppCompatActivity {
         });
 
         back.setOnClickListener(view -> {
-            Intent intent = new Intent(DetailsPostAssociation.this, FeedPostsAssociation.class);
-            intent.putExtra("id",association_user_id);
+            Intent intent = new Intent(DetailsPostAssociation.this, AssociationPage.class);
+            intent.putExtra("id", association_user_id);
             startActivity(intent);
         });
         btnForEditPost.setOnClickListener(view -> {
             Intent intent = new Intent(DetailsPostAssociation.this, EditPost.class);
-            intent.putExtra("id",association_user_id);
-            intent.putExtra("post_id",post_id);
+            intent.putExtra("id", association_user_id);
+            intent.putExtra("post_id", post_id);
             startActivity(intent);
         });
-        editpic.setOnClickListener(view -> {
+        editPic.setOnClickListener(view -> {
             Intent intent = new Intent(DetailsPostAssociation.this, picture_edit_post.class);
-            intent.putExtra("assoc_id",association_user_id);
-            intent.putExtra("post_id",post_id);
+            intent.putExtra("assoc_id", association_user_id);
+            intent.putExtra("post_id", post_id);
             startActivity(intent);
         });
     }
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.menu_bar, menu);
         return true;
     }
+
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
             case R.id.itembacktouser:
-                Intent intent1 = new Intent(DetailsPostAssociation.this,AssociationPage.class);
-                intent1.putExtra("id",association_user_id);
+                Intent intent1 = new Intent(DetailsPostAssociation.this, AssociationPage.class);
+                intent1.putExtra("id", association_user_id);
                 startActivity(intent1);
                 return true;
             case R.id.itemlogout:
